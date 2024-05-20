cde5864a4544feffe66080b957108623273c37d5
marcin1 <marcin@2loud.pl>
2024-05-20 20:36:57 +0200

    added random quotes + some ui changes

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ document.getElementById('about-me').addEventListener('click', function() {
         this.classList.remove('pop-animation');
     }, { once: true });
 });
+document.addEventListener("DOMContentLoaded", function() {
+    var quotes = [
+        "bingo bingo baby <span class='special-love-text'>I love you</span> ain't that crazy!?!?",
+    ];
+
+    var quoteElement = document.querySelector(".quote-of-the-day");
+
+    var randomIndex = Math.floor(Math.random() * quotes.length);
+
+    quoteElement.innerHTML = '"' + quotes[randomIndex] + '"'
+});
