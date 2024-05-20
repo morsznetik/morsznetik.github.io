847e2af2d5d92d49f79f39d44a36c2d5fe04a603
marcin1 <marcin@2loud.pl>
2024-05-20 21:33:03 +0200

    removed "about me" header, cleaned up some stuff, added badges

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-document.getElementById('about-me').addEventListener('click', function() {
-    this.classList.add('pop-animation');
-    this.addEventListener('animationend', function() {
-        this.classList.remove('pop-animation');
-    }, { once: true });
-});
 document.addEventListener("DOMContentLoaded", function() {
     var quotes = [
-        "bingo bingo baby <span class='special-love-text'>I love you</span> ain't that crazy!?!?",
+        "bingo bingo baby<span class='special-love-text'> I love you </span>ain't that crazy!?!?",
     ];
 
     var quoteElement = document.querySelector(".quote-of-the-day");
