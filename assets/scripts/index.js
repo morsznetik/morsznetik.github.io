b14be19561413e5285cbb10d420ff188d65988d5
marcin1 <marcin@2loud.pl>
2024-05-21 14:58:00 +0200

    new fonts, reduced glow brightness

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
     var quotes = [
         "bingo bingo baby<span class='special-love-text'> I love you </span>ain't that crazy!?!?",
+        "it ain't stupid if it works",
+        "<span class='trans-flag-text'>trans lives matter</span>"
     ];
 
     var quoteElement = document.querySelector(".quote-of-the-day");
