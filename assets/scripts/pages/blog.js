94f0eea13a5f905de60bf89cccc3e09abea14155
marcin1 <marcin@2loud.pl>
2024-05-29 09:24:57 +0200

    sidebar placed correctly(?) js working, css not done

--- assets/scripts/pages/blog.js
+++ assets/scripts/pages/blog.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     const sidebarList = document.getElementById("sidebar-list");
-    const sections = document.querySelectorAll(".blog-section");
+    const sections = document.querySelectorAll(".blog-container");
     let sidebarContent = "";
 
     sections.forEach((section) => {
         const sectionId = section.getAttribute("id");
-        const sectionTitle = section.querySelector("h2").innerText;
+        const sectionTitle = section.querySelector("h1").innerText;
         sidebarContent += `<li><a href="#${sectionId}">${sectionTitle}</a></li>`;
     });
 
